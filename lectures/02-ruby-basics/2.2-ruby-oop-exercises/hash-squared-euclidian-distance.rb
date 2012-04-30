cca3ad06bf9357bdf0d46eaab3576a252835756b
Robert Gallagher <robert@wellmadeworld.com>
2012-04-30 17:20:39 -0400

    First stab, need to understand why it doesn't work with empty hashes

--- lectures/02-ruby-basics/2.2-ruby-oop-exercises/hash-squared-euclidian-distance.rb
+++ lectures/02-ruby-basics/2.2-ruby-oop-exercises/hash-squared-euclidian-distance.rb
@@ -14,7 +14,12 @@ Add a `squared_euclidean_distance` method to the `Hash` class that takes another
 class Hash
 
   def self.squared_euclidean_distance(lhs, rhs)
-    # TODO
+
+    # For each key of both hashes, calculate the squared difference in the values
+    differences = lhs.keys.concat(rhs.keys).uniq.map{ |key| (((lhs.has_key? key)? lhs[key] : 0) - ((rhs.has_key? key)? rhs[key] : 0))**2 }
+
+    # Sum squared differences
+    if differences.empty? then 0 else differences.inject(:+) end
   end
 
   def squared_euclidean_distance(rhs)
